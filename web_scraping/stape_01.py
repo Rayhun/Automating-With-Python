b30d63ddd01fe45f2681604b81c525783a3cc444
Rayhun <rayhunkhan27@gmail.com>
2021-09-24 12:07:49 -0700

    add all data capture

--- web_scraping/stape_01.py
+++ web_scraping/stape_01.py
@@ -1,8 +1,12 @@
 from webscripe import wlog
+from webscripe import wscript
 
 wlog.set_custom_info('html/error.log')
 
-try:
-    raise Exception
-except Exception as e:
-    wlog.report(e) 
+news_scrap = wscript.NewsScraper(wscript.url_aj, wlog)
+news_scrap.retrive_webpage()
+news_scrap.write_webpage_as_html()
+news_scrap.read_webpage_from_html()
+news_scrap.convert_data_to_bs4()
+news_scrap.print_data()
+# news_scrap.parse_soup_to_simple_html()
---
+++ web_scraping/webscripe/wscript.py
@@ -0,0 +1,57 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url_aj = 'https://www.aljazeera.com'
+filepath = 'html/aj.html'
+
+
+class NewsScraper:
+    __url = ''
+    __data = ''
+    __wlog = None
+    __soup = None
+    def __init__(self, url, wlog):
+        self.__url = url
+        self.__wlog = wlog
+    
+    def retrive_webpage(self):
+        try:
+            html = urlopen(self.__url)
+        except Exception as e:
+            print(e)
+            self.__wlog.report(e)
+        else:
+            self.__data = html.read()
+            if len(self.__data) > 0:
+                print("Retrive Successfully")
+    def write_webpage_as_html(self, filepath=filepath, data=''):
+        try:
+            with open(filepath, 'wb') as fobj:
+                if data:
+                    fobj.write(data)
+                else:
+                    fobj.write(self.__data)
+        except Exception as e:
+            print(e)
+            self.__wlog.report(e)
+
+    def read_webpage_from_html(self, filepath=filepath):
+        try:
+            with open(filepath) as fobj:
+                self.__data = fobj.read()
+        except Exception as e:
+            print(e)
+            self.__wlog.report(e)
+
+    def change_url(self, url):
+        self.__url = url
+    
+    def print_data(self):
+        print(self.__data)
+    
+    def convert_data_to_bs4(self):
+        self.__soup = BeautifulSoup(self.__data, "html.parser")
+    
+    def parse_soup_to_simple_html(self):
+        news_list = self.__soup.find_all(['h1', 'h2', 'h3', 'h4'])
+        print(news_list)
