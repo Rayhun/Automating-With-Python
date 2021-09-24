5eb0f6ec1215e008e53a0b45ad34221b7ea174d5
Rayhun <rayhunkhan27@gmail.com>
2021-09-24 12:53:15 -0700

    update script

--- web_scraping/stape_01.py
+++ web_scraping/stape_01.py
@@ -4,8 +4,8 @@ from webscripe import wscript
 wlog.set_custom_info('html/error.log')
 
 news_scrap = wscript.NewsScraper(wscript.url_aj, wlog)
-# news_scrap.retrive_webpage()
-# news_scrap.write_webpage_as_html()
+news_scrap.retrive_webpage()
+news_scrap.write_webpage_as_html()
 news_scrap.read_webpage_from_html()
 news_scrap.convert_data_to_bs4()
 news_scrap.print_data()
